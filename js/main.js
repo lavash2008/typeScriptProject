2a7d0aab116e90131e140b05a1ee33bbe79f67b6
p-bayandin03@mail.ru <p-bayandin03@mail.ru>
2025-03-25 19:41:19 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var p = { x: 10, y: 20 };
+console.log(p);
+console.log(p.x);
+//# sourceMappingURL=main.js.map
---
+++ ts/app.ts
@@ -0,0 +1,6 @@
+interface Point {
+    readonly x: number;
+    readonly y: number;
+}
+let p: Point = { x: 10, y: 20 };
+console.log(p); 
